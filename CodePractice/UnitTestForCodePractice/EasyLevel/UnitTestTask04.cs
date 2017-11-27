cbf9c46d3b48dd9ffacccf26f98bf1d14b93dd1a
AlexWorkQA <alexWorkQA@gmail.com>
2017-11-27 12:43:12 +0200

    Solution for Task04

---
+++ CodePractice/UnitTestForCodePractice/EasyLevel/UnitTestTask04.cs
@@ -0,0 +1,19 @@
+﻿using NUnit.Framework;
+using CodePractice.EasyLevel;
+
+namespace UnitTestForCodePractice.EasyLevel
+{
+    [TestFixture]
+    public class UnitTestTask04
+    {
+        [Test]
+        public void PossitiveTestForTask04()
+        {
+            int velocity = 60;
+            int time = 12;
+            double expectedResult = 78.5217d;
+            double actualResult = EasyLevelTask04.GetAngleA(velocity, time);
+            Assert.AreEqual(expectedResult,actualResult,0.5d);
+        }
+    }
+}
